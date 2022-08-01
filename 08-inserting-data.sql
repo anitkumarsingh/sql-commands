a6c75fec7c983cc4564feb7fc62f1ee2d9b5e399
anitkumarsingh <anikumarsingh99@gmail.com>
2022-08-01 12:33:27 +0530

    data insertion into employer and conersions table

---
+++ 08-inserting-data.sql
@@ -0,0 +1,66 @@
+-- INSERT INTO employer(
+    -- company_name,
+    -- company_address,
+    -- yearly_renue,
+    -- is_hiring
+  -- )
+-- VALUES(
+    -- 'Learning Inc',
+    -- 'Educationstreet, 12 London',
+    -- 1.43,
+    -- TRUE
+  -- )
+
+  -- INSERT INTO employer(
+    -- company_name,
+    -- company_address,
+    -- yearly_renue,
+    -- is_hiring
+  -- )
+-- VALUES(
+    -- 'Leading Inc',
+    -- 'Education street, 09 Paris',
+    -- 124.43,
+    -- FALSE
+  -- )
+
+
+-- INSERT INTO employer(
+    -- company_name,
+    -- company_address,
+    -- yearly_renue,
+    -- is_hiring
+  -- )
+-- VALUES(
+    -- 'Big oil Inc',
+    -- 'Slipperystreet, 112 Huston',
+    -- 9.43,
+    -- TRUE
+  -- )
+
+
+-- INSERT INTO conversions(
+    -- user_name,
+    -- employer_name,
+    -- message,
+    -- date
+  -- )
+-- VALUES(
+    -- 'Max Schaciam',
+    -- 'Learning Inc',
+    -- 'A very good education company',
+    -- '2022-08-02 15:32:01'
+  -- )
+
+  INSERT INTO conversions(
+    user_name,
+    employer_name,
+    message,
+    date
+  )
+VALUES(
+    'Anit Kumar',
+    'Leading Inc',
+    'A very good education asdasdasd company',
+    '2022-08-02 19:32:01'
+  )
