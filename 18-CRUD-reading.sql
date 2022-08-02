286669475b1d37d99d53115fb26555b7da06abb7
anitkumarsingh <anikumarsingh99@gmail.com>
2022-08-02 20:05:10 +0530

    select dummy content, expression, func and alias

--- 18-CRUD-reading.sql
+++ 18-CRUD-reading.sql
@@ -1,6 +1,15 @@
 -- SELECT * FROM sales; -- all COLUMNS
 
+-- SELECT 
+-- product_name, 
+-- date_created,
+-- volume FROM sales;
+
+
 SELECT 
-product_name, 
+'Hello World',
+1233,
+product_name,
 date_created,
-volume FROM sales;
+volume /1000 AS total_sales
+FROM sales;
