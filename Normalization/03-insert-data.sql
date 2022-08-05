5e7b97f371827848c62e03f692445c6ae29d59c8
anitkumarsingh <anikumarsingh99@gmail.com>
2022-08-05 15:36:58 +0530

    more data inserted

--- Normalization/03-insert-data.sql
+++ Normalization/03-insert-data.sql
@@ -7,32 +7,54 @@
       -- ('Some street address','183A',2);
 
 
-INSERT INTO users(
-first_name,
-last_name,
-email,
-address_id
-)VALUES
-(
-  'Max',
-  'Schwarz',
-  'test@gmail.com',
-  3
-),
-(
-  'Anit',
-  'Singh',
-  'test2@gmail.com',
-  2
-),
-(
-  'John',
-  'Doe',
-  'doe@gmail.com',
-  1
-);
-
+-- INSERT INTO users(
+-- first_name,
+-- last_name,
+-- email,
+-- address_id
+-- )VALUES
+-- (
+  -- 'Max',
+  -- 'Schwarz',
+  -- 'test@gmail.com',
+  -- 3
+-- ),
+-- (
+  -- 'Anit',
+  -- 'Singh',
+  -- 'test2@gmail.com',
+  -- 2
+-- ),
+-- (
+  -- 'John',
+  -- 'Doe',
+  -- 'doe@gmail.com',
+  -- 1
+-- );
+-- 
 
 -- UPDATE users
 -- SET address_id = 1
 -- WHERE id = 3;
+
+
+
+INSERT INTO city (name)
+VALUES ('Munich'), ('Rome'), ('Tokyo'), ('Washington, D.C.');
+
+INSERT INTO address (street, house, city_id)
+VALUES 
+  ('Beerstreet', '91', 4),
+  ('Beerstreet', '12', 4),
+  ('Pizzastreet', '1', 5),
+  ('Burgerstreet', '9', 2),
+  ('Anotherstreet', '12', 1),
+  ('Smallstreet', '11', 3);
+
+
+INSERT INTO users (first_name, last_name, email, address_id)
+VALUES 
+  ('Marina', 'Marks', 'marina@test.com', 2),
+  ('Hans', 'Mayer', 'hansm@test.com', 5),
+  ('Maria', 'Marionatti', 'maria@test.com', 7),
+  ('Michael', 'Smith', 'michael@test.com', 8);
