6b4df267d7252a6deda6e3cdcc02ef763cb2d0bf
anitkumarsingh <anikumarsingh99@gmail.com>
2022-08-05 15:21:47 +0530

    multiple left join

---
+++ Normalization/07-multiple-left-join.sql
@@ -0,0 +1,6 @@
+SELECT a.id,a.street,a.house,
+c.name AS city_name,u.first_name,
+u.last_name,u.email FROM
+address AS a
+LEFT JOIN users AS u ON u.address_id = a.id
+LEFT JOIN city AS c on a.city_id = c.id ;
