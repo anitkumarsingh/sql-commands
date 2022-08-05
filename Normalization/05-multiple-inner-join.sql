7fab3b893a972588e8d8c3a5a9543b8ba1bc18d2
anitkumarsingh <anikumarsingh99@gmail.com>
2022-08-05 12:36:46 +0530

    nested inner join

---
+++ Normalization/05-multiple-inner-join.sql
@@ -0,0 +1,11 @@
+SELECT 
+u.id,
+u.first_name,
+u.last_name,
+u.email,
+a.street,
+a.house,
+c.name
+FROM users AS u 
+INNER JOIN address AS a ON u.address_id = a.id
+INNER JOIN city AS c ON a.city_id = c.id;
